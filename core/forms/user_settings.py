e110c24a2763bbe538d737bf7c26d18323b09db6
BrevonJr <romanheus1999@gmail.com>
2025-09-25 16:10:14 +0200

    More Site Restructuring/Language Toggles

--- core/forms/user_settings.py
+++ core/forms/user_settings.py
@@ -3,6 +3,13 @@ from ..models.custom_user import CustomUser
 
 
 class UserSettingsForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=20)
-    language = forms.ChoiceField(choices=CustomUser.LANG_CHOICES)
-    cursor = forms.BooleanField()
+    name = forms.CharField(label="Name",
+                           max_length=20, required=False)
+    language = forms.ChoiceField(label="Language",
+                                 choices=CustomUser.LANG_CHOICES,
+                                 required=True)
+    cursor = forms.BooleanField(label="Use Custom Cursor", required=True)
+    color = forms.ChoiceField(label="Color", required=True,
+                              choices=CustomUser.COLOR_CHOICES)
+    user_icon = forms.FileField(required=False)
+    background_image = forms.FileField(required=False)
