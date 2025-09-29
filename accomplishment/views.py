20af654fd2b7b33d4f254cf5a4601f2d45ffb554
BrevonJr <romanheus1999@gmail.com>
2025-09-29 15:12:44 +0200

    Made Alerts part of the base page

--- accomplishment/views.py
+++ accomplishment/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from core.views import render_if_logged_in
+from core.session import update_session
 from core.models.custom_user import CustomUser
 from .models.accomplishment import Accomplishment
 
 
-def overview(request, lang_code: str = "en"):
+def overview(request, lang_code: str = ""):
     """An overview of an User's Accomplishments."""
+    update_session(request=request, lang_code=lang_code)
 
     target: HttpResponse = render(
         request, "accomplishment/overview.html",
@@ -15,4 +17,4 @@ def overview(request, lang_code: str = "en"):
                 created_by=request.user)
         })
 
-    return render_if_logged_in(request, target, lang_code)
+    return render_if_logged_in(request, target)
---
+++ core/forms/user_register.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class UserRegisterForm(forms.Form):
+    username = forms.CharField(
+        label="User Name", max_length=24, required=True)
+    email = forms.EmailField(required=True)
+    password = forms.CharField(
+        label="Password", required=True,
+        min_length=8,
+        widget=forms.PasswordInput())
+    first_name = forms.CharField(
+        label="First Name", max_length=20, required=False)
+    last_name = forms.CharField(
+        label="Last Name", max_length=20, required=False)
