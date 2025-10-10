d62563109066fe48f9e740b15659a0558d78f2d5
WimpieRatte <assebliefaanvaar@gmail.com>
2025-10-10 09:25:44 +0200

    still in progress, but want to update main

--- finance/urls.py
+++ finance/urls.py
@@ -6,4 +6,8 @@ app_name = "finance"
 
 urlpatterns = [
 	path('importtransactions', views.import_transactions, name="import_transactions"),
+ 	path('importprofiles', views.import_profiles, name="import_profiles"),
+	path('import_profile_controls', views.import_profile_controls, name='import_profile_controls'),
+	path('save_import_profile', views.save_import_profile, name='save_import_profile'),
+	path('delete_import_profile', views.delete_import_profile, name='delete_import_profile'),
 ]
