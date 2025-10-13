dafdd564dbb93bf15693a16e9c3bb6a699e5f17e
BrevonJr <romanheus1999@gmail.com>
2025-10-13 23:47:49 +0200

    Safety Commit for Calendar

--- ft_calendar/templates/js_calendar_rome.js
+++ ft_calendar/templates/js_calendar_rome.js
@@ -26,3 +26,40 @@ var calendar = rome(document.getElementById('rome-calendar'),
     },
     "weekStart": current_date.getDay()
 });
+
+var calendarEntries = []
+
+let AJAXData = {csrfmiddlewaretoken: '{{ csrf_token }}'};
+$.when( basicAJAX(type="POST", url=`{% url 'calendar:get' %}`, data=AJAXData) )
+    .done(function( accompData ) {
+        calendarEntries = accompData["entries"]
+
+        // Convert all date strings to native JS Date objects
+        calendarEntries.forEach(function(entry) {
+            entry["date"] = new Date(entry["date"])
+        });
+    })
+
+function resetDayButtons(event){
+    let buttons = document.querySelectorAll(".rd-day-body");
+    let dateToCheck = calendar.getDate();
+
+    buttons.forEach(function(button) {
+        entry = calendarEntries.filter((element) => 
+        element["date"].getDate() == Number(button.textContent)
+        && element["date"].getMonth() == dateToCheck.getMonth()
+        && element["date"].getYear() == dateToCheck.getYear());
+
+        if (entry.length > 0) {
+            button.className = button.className += " position-relative";
+            button.innerHTML = `${button.innerHTML}<span class="position-absolute translate-middle badge rounded-circle bg-success fw-normal fs-sm no-ptr-evt" style="font-size: 0.7em; top: 10%; left: 90%">${entry.length}</span>`;
+        }
+    });
+
+
+    $(".rd-day-body").click(function(event) {
+        // Execute with small delay to give the calendar system time to react to the selected date
+        setTimeout(function() {updateDate(event)}, 10);
+    });
+}
+setTimeout(function() {resetDayButtons(); updateDate()}, 1000)
